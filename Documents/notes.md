b4292ae703ab9a2078c8f698e4781cff90e1d01e
Ian Wilson <nijw@newcastle.ac.uk>
2017-12-14 15:12:58 +0000

    github on the way

--- Documents/notes.md
+++ Documents/notes.md
@@ -1,3 +1,11 @@
 ## Notes
 
-For quite a few of the studies, summary statistics are available for the data
+* For quite a few of the studies, full summary statistics are available for the data.  This gives p values etc. for each SNP 
+might be useful.  This is available from teh EBI and for other databases such as the GIANT consortium, which is particularly useful for BMI and height.
+
+* Further summary statistics are available for other traits, 
+    * [GIANT consortium data files](http://portals.broadinstitute.org/collaboration/giant/index.php/GIANT_consortium_data_files).  
+    * Neale LAB and biobank summary statistics [spreadsheet](https://sites.google.com/broadinstitute.org/ukbbgwasresults/home?authuser=0).  Also includes Heritability of >2000 traits from UK biobank
+    
+
+* 
--- R/read.R
+++ R/read.R
@@ -1,9 +1,13 @@
-## Read the GWAS
+## Read in some data 
+## GWAS
+## https://www.ebi.ac.uk/gwas/
 library(here)
 source(here("R","prepare.R"))
 install.load("data.table")
 ## A list of ll associations
 a <- fread(dropbox("gwas_catalog_v1.0.1-associations_e90_r2017-12-04.tsv"))
+a2 <-  fread(dropbox("gwas-catalog-associations_ontology-annotated.tsv"))
+#a <- a2
 ## there are no cnv studies
 a$CNV <- NULL
 a$pos <- as.integer(a$CHR_POS)
@@ -32,4 +36,29 @@ table(simple, simple2)
 
 ## OK lots of the SNPs reported are reported as multiple SNPs.
 ## Read the paper/information to see what to do about this
+##
+## I believe that if multiple SNPs are reported this is because 
+## th esuthors are reporting a haplotype.
 
+## Also if the studies do not seem to have been completely entered in the database then 
+# bits may be missing.  For example a recent paper on Breast Carcinoma just has rs numbers, but no chromosome
+## ID or position.  About 1813 of the bad SNPs are like this.
+
+
+## Ancestry
+ancestry <- fread(dropbox("gwas_catalog-ancestry_r2017-12-11.tsv"))
+ancestry$V13 <- NULL
+colnames(ancestry) <- c( "STUDY_ACCCESSION",
+                         "PUBMEDID",
+                         "FIRST_AUTHOR",
+                         "DATE",
+                         "INITIAL_SAMPLE_DESCRIPTION",
+                         "REPLICATION_SAMPLE_DESCRIPTION",
+                         "STAGE",
+                          "NUMBER_OF_INDIVDUALS", 
+                         "BROAD_ANCESTRAL_CATEGORY",
+                         "COUNTRY_OF_ORIGIN", 
+                         "COUNTRY_OF_RECRUITMENT",
+                         "ADDITONAL ANCESTRY DESCRIPTION")
+hist(log10(ancestry$`NUMBER OF INDIVDUALS`))
+sort(table(ancestry$BROAD_ANCESTRAL_CATEGORY), decreasing = TRUE)[1:20]
