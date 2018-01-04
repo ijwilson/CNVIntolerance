30a7be13264d599fc5b515bd02b4cd691e9bdf6c
Ian Wilson <wilson.ianj@gmail.com>
2018-01-04 12:07:06 +0000

    add some files

--- R/VUR.R
+++ R/VUR.R
@@ -1,20 +1,52 @@
-## VUR
 
 ## PRISM is available at http://compbio.cs.toronto.edu/prism
 
 library(here)
 source(here("R","prepare.R"))
 install.load("data.table")
+install.load.bioc("GenomicRanges", "GenomicAlignments")
 
 del <- fread(dropbox("VUR/DEL_results.txt"))
 dup <- fread(dropbox("VUR/DUP_results.txt"))
+
+del_filter <- del[left_match>60 & right_match > 60]
+
+conv <- function(xx) {
+  g <- GRanges(seqnames=xx$X.chr, 
+               IRanges(start=xx$gap_start, end=xx$gap_end)
+               , ID=xx$ID
+               , type=xx$type
+               , support = xx$support_read_num
+               , left_match = xx$left_match
+               ,right_match = xx$right_match)
+}
+
+
+gdel <- conv(del_filter)
+cov_gdel <- coverage(gdel)
+## split by ID
+sp <- split(gdel, gdel$ID)
+## just get the overlaps.  
+sp_reduce <- lapply(sp, reduce)
+id_names <- names(sp_reduce)
+spb <- mapply(function(x,y) {x$ID <- y; return(x)}, sp_reduce, id_names)
+
+
+r <- Reduce(c, spb)
+length(r)
+
+cov2 <- coverage(r)
+peaks <- slice(cov2$chr10, lower=24)
+length(peaks)
+peaksb <- peaks[width(peaks)>20]
+gdup <- conv(dup)
+
 
 
 table(dup$ID)
 
 hist(table(del$ID))
 
-plot(del$gap_start)
 
 
 ## One approach here mght be to go along the chromosome and count the total number of individuals that 
