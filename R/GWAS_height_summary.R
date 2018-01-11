4b5ac7092f83231c6ac315f014e91dd3dc389cf8
Ian Wilson <wilson.ianj@gmail.com>
2018-01-11 07:37:54 +0000

    work on the height GWAS

--- R/GWAS_height_summary.R
+++ R/GWAS_height_summary.R
@@ -10,13 +10,38 @@ GWAS_height_summary <- read.table(
   header=TRUE, stringsAsFactors = FALSE)
 head(GWAS_height_summary)
 
-snps <- SNPlocs.Hsapiens.dbSNP144.GRCh37
-#gr <- rsidsToGRanges(snps, GWAS_height_summary$MarkerName[1:20])
+cat("There are", nrow(GWAS_height_summary), "rows in the summary statistics.\n")
 
-t100    <- system.time(gr <- snpsById(snps, GWAS_height_summary$MarkerName[1:100]))
-t1000   <- system.time(gr <- snpsById(snps, GWAS_height_summary$MarkerName[1:1000]))
+GWAS_height_summary <- GWAS_height_summary[substring(GWAS_height_summary$MarkerName, 1, 2)=="rs", ]
+
+snps <- SNPlocs.Hsapiens.dbSNP144.GRCh37
+
 gr <- snpsById(snps, GWAS_height_summary$MarkerName, ifnotfound="warning")
 
 GWAS_height_summary <- GWAS_height_summary[GWAS_height_summary$MarkerName %in% gr$RefSNP_id,]
-table(GWAS_height_summary$MarkerName== gr$RefSNP_id)
+
+cat("after leaving only those with positions, there are", 
+    nrow(GWAS_height_summary), "rows in the summary statistics.\n")
 GWAS_height_summary <- cbind(GWAS_height_summary, gr)
+
+if (!dir.exists(here("output"))) {
+  dir.create(here("output"))
+}
+GWAS_height_summary$strand <- NULL
+GWAS_height_summary$RefSNP_id <- NULL
+GWAS_height_summary$alleles_as_ambig <- NULL
+GWAS_height_summary <- GWAS_height_summary[!(GWAS_height_summary$seqnames %in% c("MT","X","Y")),]
+GWAS_height_summary$seqnames <- as.numeric(GWAS_height_summary$seqnames)
+
+save(GWAS_height_summary, file=here("output", "GWAS_height_summary.rda"))
+
+#install.load("ggplot2")
+#ggplot(GWAS_height_summary, aes(x=b)) + geom_density() + xlim(-0.05, 0.05)
+plot(density(GWAS_height_summary$b))
+
+install.load("qqman")
+manhattan(GWAS_height_summary, chr="seqnames", bp="pos", p="p", snp="MarkerName" )
+
+qq(GWAS_height_summary)
+
+
