d15c3f8aa699bcc2e54c458062cc657145c857f4
Ian Wilson <nijw@newcastle.ac.uk>
2018-03-20 15:16:24 +0000

    more support for the work with corcordant signals

---
+++ R/GWAS_summary_intelligence.R
@@ -0,0 +1,24 @@
+## Genome-wide association meta-analysis of 78,308 individuals identifies new loci and genes influencing
+## human intelligence
+## Sniekers et al Nature Genetics volume 49, pages 1107–1112 (2017) doi:10.1038/ng.3869
+#' https://www.nature.com/articles/ng.3869
+#' 
+
+library(here)
+source(here("R","prepare.R"))
+
+install.load("data.table", "qqman")
+
+## neuroticism summary results from my own computer
+a <- fread("D:/data/GWAS_Summary/Intelligence/Sniekers.txt")
+
+head(a)
+
+png(height=500, width=1200, file=here("figs","intelligence.png"))
+
+manhattan(a, bp="position", chr="Chromosome", p="p_value", snp="rsid")
+
+dev.off()
+
+
+
---
+++ R/concordance3.R
@@ -0,0 +1,51 @@
+#' ## 3 way SNP analysis
+
+library(here)
+source(here("R","prepare.R"))
+install.load("data.table")
+load(here("output", "snps_SHN.rda"))
+
+#' ## Simplfy things to get some simple statistics and plots
+
+z <- data.frame(scz = snp_shn$zb.scz, height=snp_shn$zb.height, neur = snp_shn$zb.neu) 
+
+gene_z <- split(z, snp_shn$GENEID)
+gene_mad <- by(z, snp_shn$GENEID, function(x) colMeans(abs(x)))
+gene_m <- matrix(unlist(gene_mad), ncol=3, byrow=TRUE)
+rownames(gene_m) <- names(gene_mad)
+colnames(gene_m) <- c("scz", "height", "neur")
+gene_n <- tapply(snp_shn$GENEID, snp_shn$GENEID, length)
+gene_cor <- lapply(gene_z, cor)
+
+n_agree <- t(sapply(gene_z, function(x) apply(x, 2, function(z) sum(abs(z)>2))))
+
+o <- as.vector(t(outer(c("s","","S"), c("h", "", "H"), paste0)))
+levels <- as.vector(t(outer(as.vector(t(o)), c("n","","N"), paste0)))
+
+
+classify <- function(zz) {
+  zc <- apply(zz, 2, cut, c(-Inf, -2, 2, Inf), labels=FALSE)
+  1+zc[,1]-1 + 3*(zc[,2]-1) + 9*(zc[,3]-1)
+}
+
+z_cut <- classify(z)
+z_cut <- factor(z_cut, labels=levels)
+table( scz=cut(z$scz, c(-Inf, -2, 2, Inf)),height= cut(z$height, c(-Inf,-2,2,Inf)))
+table( scz=cut(z$scz, c(-Inf, -2, 2, Inf)), neuroticism=cut(z$neur, c(-Inf,-2,2,Inf)))
+table( height=cut(z$height, c(-Inf, -2, 2, Inf)), neuroticism=cut(z$neur, c(-Inf,-2,2,Inf)))
+
+## This needs to be done by gene or there is little point.
+
+
+tb <- table(z_cut)
+tb2 <- data.frame(tb[1:13],rev(tb[15:27]))
+ tb4 <- data.frame( changes = c(paste(tb2[,3],paste(tb2[,1]),sep="/"), "none"), count= c(rowSums(tb2[,c(2,4)]), tb[14]))
+
+
+
+tbtbsum((x$scz > 2 & x$height > 2 & x$neur > 2) | (x$scz < -2 & x$height < -2 & x$neur < -2)
+  ))
+
+length(gene_n)
+colMeans(z)
+
